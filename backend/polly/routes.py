472d596189b2755e5c9c1717d1a3117fdeee1dd8
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-07-30 17:01:08 +0530

    Implemented `My Polls`, made itthe app truly single page

--- backend/polly/routes.py
+++ backend/polly/routes.py
@@ -24,9 +24,10 @@ def login():
     else:
         return make_response(jsonify(message='Invalid credentials'), 401)
 
-@app.route('/api/poll/<int:poll_id>/')
+@app.route('/api/create')
 @jwt_required()
-def getPoll(poll_id):
+def create_poll(poll_id):
+    poll = db.create_poll(get_jwt_identity(), request.json.get('title'), request.json.get('expire'), request.json.get('qns'))
     poll = db.get_qns(get_jwt_identity(), poll_id)
     return make_response(jsonify(poll), 200)
 
@@ -35,3 +36,9 @@ def getPoll(poll_id):
 def get_polls():
     polls = db.get_polls(get_jwt_identity())
     return make_response(jsonify(polls), 200)
+
+@app.route('/api/poll/<int:poll_id>/')
+@jwt_required()
+def get_poll(poll_id):
+    poll = db.get_qns(get_jwt_identity(), poll_id)
+    return make_response(jsonify(poll), 200)
